c87f16cf2336c463e0259e3ab98e29ad4f166d58
bakerkai <bakerkai@urbandaleschools.com>
2021-09-23 10:33:19 -0500

    working on grading

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -11,4 +11,10 @@ Test with the following:
 Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
+
+
+OG = (SG*(100-FW) + FE*FW)/100
+grade times worth added together
 '''
+print("Welcome to grading software v1.1")
+SG = int(input)
