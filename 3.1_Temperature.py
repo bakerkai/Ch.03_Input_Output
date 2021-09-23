c7f300aad63dae429b9efe6960ffa4d44a0a2ef2
bakerkai <bakerkai@urbandaleschools.com>
2021-09-23 10:26:38 -0500

    temperature

--- 3.1_Temperature.py
+++ 3.1_Temperature.py
@@ -10,9 +10,12 @@ In: 212  Out: 100
 In: 52  Out: 11.1
 In: 25  Out: -3.9
 In: -40  Out: ??? Please tell me what this output is!
-
+-40.0
 '''
-
+print("Hello, this is a program to turn Fahrenheit into Celsius")
+Fahrenheit = int(input("What is the temperature in Fahrenheit?"))
+Celsius = (Fahrenheit - 32) * 5.0/9.0
+print(Celsius)
 
 
 
