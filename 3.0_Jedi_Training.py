f3e33a5ff6df2c0d0bc5aebc0774137e12cf1e08
bakerkai <bakerkai@urbandaleschools.com>
2021-10-11 10:10:25 -0500

    12123

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -2,34 +2,34 @@
 # In all the short programs below, do a good job communicating with your end user!
 import math
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
-#name = input("What is your name?")
-#print("Nice to meet you,", name )
+name = input("What is your name?")
+print("Nice to meet you,", name )
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
-#print("Hello! Welcome to the area finder of a triangle V12.1921(alpha) (unstable build)")
-#height = int(input("What is the height of the triangle?"))
-#base = int(input("What is the base of the triangle?"))
-#area = base * height * 0.5
-#print("Your answer is,", area)
+print("Hello! Welcome to the area finder of a triangle V12.1921(alpha) (unstable build)")
+height = int(input("What is the height of the triangle?"))
+base = int(input("What is the base of the triangle?"))
+area = base * height * 0.5
+print("Your answer is,", area)
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
-#radius = int(input("What is the radius of the circle?"))
-#circumference = radius * 2 * 3.14
-#print(circumference)
+radius = int(input("What is the radius of the circle?"))
+circumference = radius * 2 * math.pi
+print(circumference)
 
 # 4. Ask a user for an integer and then print the square root.
-#number = int(input("What ist he integer?"))
-#finalanswer = number / number
-#print(finalanswer)
+number = int(input("What ist he integer?"))
+finalanswer = math.sqrt(number)
+print(finalanswer)
 
 
 
 
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
-#mass = int (input("What is the mass of the object?"))
-#acceleration = int(input("What is the acceleration?"))
-#answer = mass * acceleration
-#print(answer, "May the accleration times mass be with you!")
+mass = int (input("What is the mass of the object?"))
+acceleration = int(input("What is the acceleration?"))
+answer = mass * acceleration
+print(answer, "May the accleration times mass be with you!")
 
 
